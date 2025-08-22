62021f7f1abfa1ede70641c4a5243eea3ff7265c
harish <harishappana@gmail.com>
2025-08-22 19:14:33 +0530

    added login feature

---
+++ auth.js
@@ -0,0 +1,5 @@
+# write basic auth login
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { useAuth } from '../context/AuthContext';
+import { toast } from 'react-toastify';
